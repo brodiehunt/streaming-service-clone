030fa09ade16e756a37a0c2b6e50897092d7123a
Brodie Hunt <62501585+brodiehunt@users.noreply.github.com>
2024-11-21 21:50:48 +1100

    Define Zod Schemas for login and signup forms

---
+++ src/lib/zodSchemas.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod'
+
+export const signinSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+})
+
+export const signupSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+  confirmPassword: z.string().min(8),
+  givenName: z.string().min(2).max(50),
+  familyName: z.string().min(2).max(50),
+  gender: z.string().min(2).max(10),
+})
+
+export const emailSchema = z.string().email()
