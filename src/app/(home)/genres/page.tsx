bc343d1d57e3079a27fbe6affff13933d57f9b1b
Brodie Hunt <62501585+brodiehunt@users.noreply.github.com>
2024-11-27 19:07:34 +1100

    Create genres route and page component.

---
+++ src/app/(home)/genres/page.tsx
@@ -0,0 +1,10 @@
+export default async function GenresPage() {
+  return (
+    <section className="mt-[100px] xl:mt-[130px] px-layout-x-large relative">
+      <h1 className="text-center font-bold text-2xl md:text-4xl xl:text-6xl text-white">
+        Categories
+      </h1>
+      <section className=""></section>
+    </section>
+  )
+}
